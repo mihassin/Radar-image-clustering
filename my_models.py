a060ba3d07527bb30311b364eb4f38dd01660fb8
mihassin <marko.hassinen@helsinki.fi>
2016-02-15 12:35:05 +0200

    yesterdays stuff and todays new stuff

---
+++ my_models.py
@@ -0,0 +1,12 @@
+class my_models:
+	
+	def __init__(self):
+		self.nothing = 0
+
+	def getNothing(self):
+		return self.nothing
+
+	def k_means(self):	
+
+	def k_metoids(self):
+		return "asd"
--- preprocessor.py
+++ preprocessor.py
@@ -4,14 +4,49 @@ import numpy as np
 import os
 
 
-def count_data(data):
-	return len(data)
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+' Class preprocessor                                               '
+' attributes:                                                      '
+'  - data,   array containing image pixel arrays                   '
+'  - amount, amount of images                                      '
+'  - path,   path where images where gathered                      '
+' function:                                                        '
+'  - count_data, returns the amount of images                      '
+'  - get_path, returns path as string                              '
+'  - get_data, returns an array containing images as pixel arrays  '
+'  - __read_data, private function to gather data                  '
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 
-def read_data(root):
-	data = []
-	for root, dirs, files in os.walk(root):
-		for file in files:
-			img = mpimg.imread(os.path.join(root,file))
-			data.append(mpimg.imread(os.path.join(root,file)))
-	return data
+class preprocessor:
+
+	def __init__(self):
+		self.__data = []
+		self.__amount = 0
+		self.__path = ""
+
+
+	def count_data(self):
+		return self.__amount
+
+
+	def get_path(self):
+		return self.__path
+
+
+	def get_data(self, path):
+		if self.count_data() == 0 or self.__path != path:
+			self.__path = path
+			self.__data = self.__read_data()
+			self.__amount = len(self.__data)
+		return self.__data
+
+
+	#data/pics
+	def __read_data(self):
+		data = []
+		for root, dirs, files in os.walk(self.__path):
+			for file in files:
+				img = mpimg.imread(os.path.join(root,file))
+				data.append(mpimg.imread(os.path.join(root,file)))
+		return data
