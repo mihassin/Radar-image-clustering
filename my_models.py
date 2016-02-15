708ded8942cda6c184c3a9fc8589e386d138b771
mihassin <marko.hassinen@helsinki.fi>
2016-02-15 14:03:40 +0200

    going for a break

--- my_models.py
+++ my_models.py
@@ -1,12 +1,44 @@
+import numpy.random as rand
+from preprocessor import *
+
+
 class my_models:
 	
 	def __init__(self):
-		self.nothing = 0
+		self.__preprocessor = preprocessor()
+		self.__training_data = []
+		self.__means = []
 
-	def getNothing(self):
-		return self.nothing
 
-	def k_means(self):	
+	def get_cluster_means(self):
+		return self.__means
+
+
+	def get_training_data(self, path):
+		self.__training_data = self.__preprocessor.get_data(path)
+		return "done"
+
+
+	def __randomly_init_cluster_means(self, k):
+		n = self.__preprocessor.count_data()
+		for i in range(k):
+			img = self.__training_data[rand.randint(n)]
+			self.__means.append(img)
+		return self.__means
+
+
+	def k_means(self, k):
+		self.__randomly_init_cluster_means(k)
+		while True:
+			clusterify()
+			tmp_means = []
+			for j in range(k):
+				cluster_j = []
+				newmean_j
+			if self.__means == tmp_means:
+				break
+		return self.__means
+
 
 	def k_metoids(self):
-		return "asd"
+		return "TODO"
--- preprocessor.py
+++ preprocessor.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import numpy as np
 import os
 
 
