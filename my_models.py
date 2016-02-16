891337f94ef96aa4edb07fa63888055339ba8288
mihassin <marko.hassinen@helsinki.fi>
2016-02-16 18:34:51 +0200

    k-metoids

--- my_models.py
+++ my_models.py
@@ -1,3 +1,4 @@
+import numpy as np
 import numpy.random as rand
 from preprocessor import *
 
@@ -27,18 +28,57 @@ class my_models:
 		return self.__means
 
 
+	def __clusterify():
+		return
+
+
+	def __divide_data():
+		return
+
+
+	def __newmean(self, subset):
+		mean = np.sum(subset, axis=0)
+		mean /= len(subset)
+		return mean
+
+
 	def k_means(self, k):
+		clusters = dict()
 		self.__randomly_init_cluster_means(k)
 		while True:
-			clusterify()
+			clusters = __clusterify()
 			tmp_means = []
 			for j in range(k):
-				cluster_j = []
-				newmean_j
+				subset = divide_data()
+				tmp_means[j] = newmean(subset)
 			if self.__means == tmp_means:
 				break
 		return self.__means
 
+	#requires modifications
+	def __find_subset_minimum(self, dis_X, subset):
+    	mu_key = -1
+    	mu_dist = 1.0e12
+    	tmp_dist = 0
+    	for i in subset:
+        	for j in subset:
+            	tmp_dist += dis_X[i][j] 
+        	if mu_dist > tmp_dist:
+            	mu_key = i
+            	mu_dist = tmp_dist
+    	return X[mu_key]
 
-	def k_metoids(self):
-		return "TODO"
+
+	def k_metoids(self, k):
+		clusters = dict()
+		self.__randomly_init_cluster_means(k)
+		while True:
+			clusters = __clusterify()
+			tmp_means = []
+			for j in range(k):
+				subset = self.__subset(clusters, j)
+				tmp_means[k] == self.__find_subset_minimum(subset)
+			if self.__means == tmp_means:
+				break
+			self.__means = tmp_means
+		return self.__means
