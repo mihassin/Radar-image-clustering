f69913c5fc02f92f8351e04e4be6fc3e48504013
mihassin <marko.hassinen@helsinki.fi>
2016-03-07 16:28:20 +0200

    trying to separate heroku demo

--- weatherapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, world. Only two kinds of weather types excist: Clear and Non-clear.")
