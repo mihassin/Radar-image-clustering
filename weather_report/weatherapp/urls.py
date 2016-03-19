9e3c980f93f5f2e83b5f0fe7c8aba01a90d3cd9c
mihassin <marko.hassinen@helsinki.fi>
2016-03-19 20:00:32 +0200

    Update urls.py

--- weather_report/weatherapp/urls.py
+++ weather_report/weatherapp/urls.py
@@ -1,1 +1,15 @@
-views.py
+from django.conf.urls import url, patterns
+
+from . import views
+
+urlpatterns = patterns('',
+    #/weatherapp
+    url(r'^$', views.index, name='index'),
+    url(r'^kmeans/$', views.kmeans, name='kmeans'),
+    url(r'^kmeanspp/$', views.kmeanspp, name='kmeanspp'),
+    url(r'^kmedoids/$', views.kmedoids, name='kmedoids'),
+
+)
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
